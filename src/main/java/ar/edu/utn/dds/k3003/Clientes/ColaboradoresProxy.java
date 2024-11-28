1c9273bcad9bd550bc1f536d3f076ed57da2c133
FedericoBruni <fbruni@fi.uba.ar>
2024-11-28 16:33:38 -0300

    arreglos
    co-authored-by: Julian-Mosquera <jumosquera@frba.utn.edu.ar>

--- src/main/java/ar/edu/utn/dds/k3003/Clientes/ColaboradoresProxy.java
+++ src/main/java/ar/edu/utn/dds/k3003/Clientes/ColaboradoresProxy.java
@@ -1,4 +1,4 @@
-package ar.edu.utn.dds.k3003.clients;
+package ar.edu.utn.dds.k3003.Clientes;
 
 import ar.edu.utn.dds.k3003.Clientes.ColaboradoresRetrofitClient;
 import ar.edu.utn.dds.k3003.facades.dtos.TrasladoDTO;
